db5b737dc9900020a91bbfb067be53a9fbdcba8c
Alexey Petin <alex_beholder@mail.ru>
2023-05-21 22:25:16 +0300

    Initial commit

---
+++ src/main/java/BinOps.java
@@ -0,0 +1,26 @@
+public class BinOps {
+
+    public String sum(String a, String b) {
+    /*Переведите числа из двоичной записи в
+      строках в int, сложите и переведите
+      обратно в двоичную запись*/
+        String res = null;
+        try{
+            res = Integer.toBinaryString(Integer.parseInt(a, 2) + Integer.parseInt(b, 2));
+        }
+        catch (NumberFormatException e){}
+        return res;
+    }
+
+    public String mult(String a, String b) {
+    /*Переведите числа из двоичной записи в
+      строках в int, перемножьте и переведите
+      обратно в двоичную запись*/
+        String res = null;
+        try {
+            res = Integer.toBinaryString(Integer.parseInt(a, 2) * Integer.parseInt(b, 2));
+        }
+        catch (NumberFormatException e){}
+        return res;
+    }
+}
